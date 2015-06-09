0aa7625f9f156faf1adbf9e60466b4d108210d6b
Matt Surabian <matt@mattsurabian.com>
2015-06-09 12:46:38 -0400

    Add full dependency chain

--- vendor.sh
+++ vendor.sh
@@ -38,7 +38,10 @@ clone() {
 }
 
 # List Project Dependencies
+clone hg code.google.com/p/go.crypto 69e2a90ed92d
+
 clone git github.com/mattsurabian/msg c329a42586fca968e152a235c3a155b10819fa78
 clone git github.com/mitchellh/cli 8230c3f351c1efa17429df4e771ab8dcd67ff4bd
 clone git github.com/andrew-d/go-termutil 91702f30b7f6d63f574b486457bae6acb1534dce
 clone git github.com/rakyll/globalconf 415abc325023f1a00cd2d9fa512e0e71745791a2
+clone git github.com/rakyll/goini 907cca0f578a5316fb864ec6992dc3d9730ec58c
